3437117c89fdc7f85f634f2c54e0b0098220ff12
Samra-Mushtaq <samramushtaq077@gmail.com>
2022-08-24 07:00:53 -0700

    naming conventions, breeze installation , model controller creation

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 // Route::get('/a', function () {
     // return view('dashboard');
-// });
+// }); //
 
 Route::get('/dashboard', function () {
     return view('backend.dahboard.app');
